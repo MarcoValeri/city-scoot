fcb80ced0999d0b22a4591df8b6a0c698788dc6e
MarcoValeri <marcovaleri@hotmail.it>
2020-12-29 19:27:59 +0000

    Display data into guides.html in alphabetical order by guides surname

--- scripts/get-guides.js
+++ scripts/get-guides.js
@@ -7,32 +7,37 @@
     firebase.initializeApp(firebaseConfig);
 
     const guideRef = firebase.database().ref().child("guides");
-    guideRef.on("child_added", snap => {
+    guideRef
+    .orderByChild("guide_last_name")
+    .on("child_added", snap => {
 
-        let guideID = snap.child("guideID").val();
-        let guideName = snap.child("guideName").val();
-        let guideLastName = snap.child("guideLastName").val();
-        let guideAge = snap.child("guideAge").val();
-        let guideOccupation = snap.child("guideOccupation").val();
-        let guideBlueBadge = snap.child("guideBlueBadge").val();
-        let guideTour = snap.child("guideTour").val();
-        let guideImg = snap.child("guideImg").val();
+
+        let guideID = snap.child("guide_ID").val();
+        let guideName = snap.child("guide_name").val();
+        let guideLastName = snap.child("guide_last_name").val();
+        let guideAge = snap.child("guide_age").val();
+        let guideOccupation = snap.child("guide_occupation").val();
+        let guideBlueBadge = snap.child("guide_blue_badge").val();
+        let guideTour = snap.child("guide_tour").val();
+        let guideImg = snap.child("guide_img").val();
 
         $("#guides-output").append(
             `
-            <div class="card border-dark mb-3">
-                <img class="card-img-top" src="../images/guide-placeholder-image.jpg" alt="Card image cap">
-                <div class="card-body text-info">
-                    <h5 class="card-title">Guide ${guideID}</h5>
-                    <p class="card-text">
-                        - Name: ${guideName} ${guideLastName}<br>
-                        - Age: ${guideAge}<br>
-                        - Tours: ${guideTour}<br>
-                        - Occupation: ${guideOccupation}<br>
-                        - Blue Badge: ${guideBlueBadge}<br>
-                    </p>
+            <section class="col-xs-12 col-sm-6 col-lg-3">
+                <div class="card border-dark mb-3">
+                    <img class="card-img-top" src="../images/${guideImg}" alt="Card image cap">
+                    <div class="card-body text-info">
+                        <h5 class="card-title">Guide ${guideID}</h5>
+                        <p class="card-text">
+                            - Name: ${guideName} ${guideLastName}<br>
+                            - Age: ${guideAge}<br>
+                            - Tours: ${guideTour}<br>
+                            - Occupation: ${guideOccupation}<br>
+                            - Blue Badge: ${guideBlueBadge}<br>
+                        </p>
+                    </div>
                 </div>
-            </div>
+            </section>
             `
         );
 
